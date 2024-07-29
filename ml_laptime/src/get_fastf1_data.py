50375821a0c411768b18ae32975efd16f3a47dca
victorien <victorienmichel3@gmail.com>
2024-07-29 14:58:47 +0200

    FEAT: pass year as argument in cli

--- ml_laptime/src/get_fastf1_data.py
+++ ml_laptime/src/get_fastf1_data.py
@@ -1,20 +1,32 @@
 import fastf1 as ff1
 import pickle
+from pathlib import Path
+from argparse import ArgumentParser
 
-ff1.Cache.enable_cache('.')
-year = 2018
-schedule = ff1.get_event_schedule(year).query(
-    'EventFormat == "conventional"'
-)['EventName'].values
+parser = ArgumentParser()
+parser.add_argument("year", type=int)
+year = parser.parse_args().year
+
+cache = Path("cache")
+cache.mkdir(exist_ok=True)
+ff1.Cache.enable_cache(cache)
+
+schedule = (
+    ff1.get_event_schedule(year)
+    .query('EventFormat == "conventional"')["EventName"]
+    .values
+)
 
 sessions = {}
 for i, gp in enumerate(schedule):
-    session = ff1.get_session(year, i + 1, 'R')
+    session = ff1.get_session(year, i + 1, "R")
     session.load()
-    ver_laps = session.laps.pick_driver('VER')
-    sessions.setdefault(gp, {})['laps'] = ver_laps
-    sessions[gp]['weather'] = session.weather_data
-    sessions[gp]['telemetry'] = ver_laps.telemetry
+    ver_laps = session.laps.pick_driver("VER")
+    sessions.setdefault(gp, {})["laps"] = ver_laps
+    sessions[gp]["weather"] = session.weather_data
+    sessions[gp]["telemetry"] = ver_laps.telemetry
 
-with open(f'races_{year}.pkl', 'wb') as f:
+with open(f"races_{year}.pkl", "wb") as f:
     pickle.dump(sessions, f)
+
+cache.rmdir()
