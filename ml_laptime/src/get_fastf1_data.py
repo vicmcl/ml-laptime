ad1adfe7c066e70989def0f538d249d1eb0e40c5
victorien <victorienmichel3@gmail.com>
2024-07-31 17:19:26 +0200

    FEAT: include track status in data

--- ml_laptime/src/get_fastf1_data.py
+++ ml_laptime/src/get_fastf1_data.py
@@ -1,32 +1,37 @@
 import fastf1 as ff1
 import pickle
 from pathlib import Path
-from argparse import ArgumentParser
+import shutil
 
-parser = ArgumentParser()
-parser.add_argument("year", type=int)
-year = parser.parse_args().year
+years = [2019, 2020, 2021]
+driver = "VER"
 
 cache = Path("cache")
 cache.mkdir(exist_ok=True)
 ff1.Cache.enable_cache(cache)
 
-schedule = (
-    ff1.get_event_schedule(year)
-    .query('EventFormat == "conventional"')["EventName"]
-    .values
+gp_list = set.intersection(
+    *(set(
+        ff1.get_event_schedule(year)
+        .query('EventFormat == "conventional"')["EventName"]
+        .values
+    ) for year in years)
 )
+gp_list = list(gp_list)
+print(gp_list)
 
-sessions = {}
-for i, gp in enumerate(schedule):
-    session = ff1.get_session(year, i + 1, "R")
-    session.load()
-    ver_laps = session.laps.pick_driver("VER")
-    sessions.setdefault(gp, {})["laps"] = ver_laps
-    sessions[gp]["weather"] = session.weather_data
-    sessions[gp]["telemetry"] = ver_laps.telemetry
+for year in years:
+    sessions = {}
+    for gp in gp_list:
+        session = ff1.get_session(year, gp, "R")
+        session.load()
+        ver_laps = session.laps.pick_driver(driver)
+        sessions.setdefault(gp, {})["laps"] = ver_laps
+        sessions[gp]["weather"] = session.weather_data
+        sessions[gp]["telemetry"] = ver_laps.telemetry
+        sessions[gp]["status"] = session.track_status
 
-with open(f"races_{year}.pkl", "wb") as f:
-    pickle.dump(sessions, f)
+    with open(f"races_{year}.pkl", "wb") as f:
+        pickle.dump(sessions, f)
 
-cache.rmdir()
+shutil.rmtree(cache)
