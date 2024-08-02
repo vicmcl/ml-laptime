6822fcb526a2cadc17926df6b8fe0667b363b0be
victorien <victorienmichel3@gmail.com>
2024-08-02 18:07:35 +0200

    FEAT:  create a module for utility functions

---
+++ ml_laptime/src/utils.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+def map_drs(drs):
+    if drs >= 10:
+        return True
+    else:
+        return False
+
+
+def remove_pitstops(laps):
+    laps_data = laps.copy()
+    mask = pd.isnull(laps_data['PitOutTime']) & pd.isnull(laps_data['PitInTime'])
+    laps_data = laps_data[mask]
+    laps_data = laps_data.drop(['PitOutTime', 'PitInTime'], axis=1)
+    return laps_data
+
+
+def laps_with_incident(laps, incident_times, allclear_times):
+    laps_to_remove = []
+    # Loop over every incident
+    for start_incident, end_incident in zip(incident_times, allclear_times):
+        i = 0
+        # Find the first impacted lap
+        while laps.iloc[i]['Time'] < start_incident:
+            i += 1
+        laps_to_remove.append(laps.iloc[i]['Time'])
+
+        # If the incident is not over by the end of the lap, remove the next lap
+        # and repeat until the incident is over
+        while laps.iloc[i]['Time'] < end_incident:
+            laps_to_remove.append(laps.iloc[i + 1]['Time'])
+            i += 1
+    return laps_to_remove
+
+
+def remove_incidents(laps, track_status):
+    laps_data = laps.copy()
+    incident_times = track_status[track_status['Incident'] == True]['Time'].tolist()
+    allclear_times = track_status[track_status['Incident'] == False]['Time'].iloc[1:].tolist()
+    laps_to_remove = laps_with_incident(laps_data, incident_times, allclear_times)
+    laps_data = laps_data[~laps_data["Time"].isin(laps_to_remove)]
+    return laps_data
