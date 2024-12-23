4ea09b33a1a560d5206f7ca748cecaeca0b1e31c
vicmcl <victorienmichel3@gmail.com>
2024-12-23 19:42:49 +0100

    FEAT: spllit the code in basic fucntions

--- ml_laptime/src/backfill.py
+++ ml_laptime/src/backfill.py
@@ -3,35 +3,50 @@ import pickle
 from pathlib import Path
 import shutil
 
-years = [2019, 2020, 2021]
-driver = "VER"
+YEARS = [2019, 2020, 2021]
+DRIVER = "VER"
+DATA_PATH = Path("../data")
+CACHE_PATH = Path("cache")
 
-cache = Path("cache")
-cache.mkdir(exist_ok=True)
-ff1.Cache.enable_cache(cache)
 
-gp_list = set.intersection(
-    *(set(
+def set_cache(cache_path=CACHE_PATH):
+    if cache_path.exists():
+        shutil.rmtree(cache_path)
+    cache_path.mkdir(exist_ok=True)
+    ff1.Cache.enable_cache(cache_path)
+
+
+def intersect_schedules(years=YEARS):
+    schedules = [
         ff1.get_event_schedule(year)
-        .query('EventFormat == "conventional"')["EventName"]
-        .values
-    ) for year in years)
-)
-gp_list = list(gp_list)
-print(gp_list)
+        .query('EventFormat == "conventional"')["EventName"].values
+        for year in years
+    ]
+    gp_list = set.intersection(*(set(gp_season) for gp_season in schedules))
+    gp_list = list(gp_list)
+    return gp_list
 
-for year in years:
-    sessions = {}
-    for gp in gp_list:
-        session = ff1.get_session(year, gp, "R")
-        session.load()
-        ver_laps = session.laps.pick_driver(driver)
-        sessions.setdefault(gp, {})["laps"] = ver_laps
-        sessions[gp]["weather"] = session.weather_data
-        sessions[gp]["telemetry"] = ver_laps.telemetry
-        sessions[gp]["status"] = session.track_status
 
-    with open(f"races_{year}.pkl", "wb") as f:
-        pickle.dump(sessions, f)
+def main():
+    set_cache()
+    gp_list = intersect_schedules()
 
-shutil.rmtree(cache)
+    for year in YEARS:
+        sessions = {}
+        for gp in gp_list:
+            session = ff1.get_session(year, gp, "R")
+            session.load()
+            driver_laps = session.laps.pick_driver(DRIVER)
+            sessions.setdefault(gp, {})["laps"] = driver_laps
+            sessions[gp]["weather"] = session.weather_data
+            sessions[gp]["telemetry"] = driver_laps.telemetry
+            sessions[gp]["status"] = session.track_status
+
+        with open(DATA_PATH / f"races_{year}.pkl", "wb") as f:
+            pickle.dump(sessions, f)
+
+    shutil.rmtree(CACHE_PATH)
+
+
+if __name__ == "__main__":
+    main()
